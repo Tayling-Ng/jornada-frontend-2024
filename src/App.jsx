cac7fda1aa79c109dd72967ef282dde671dcf425
Tayling Ng <taymng@yahoo.com.br>
2024-01-30 14:00:50 +0000

    configuração da props

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,26 @@ import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
+  const item1 = {
+    name: 'Rick Sanchez',
+    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  }
+
+  const item2 = {
+    name: 'Morty Smith',
+    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  }
+
+  const item3 = {
+    name: 'Summer Smith',
+    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+  }
+
   return (
     <>
-      <Card />
-      <Card />
+      <Card item={item1} />
+      <Card item={item2} />
+      <Card item={item3} />
     </>
   )
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,19 +1,25 @@
-function Card() {
-    return (
-        <>
-          <div className="card">
-            <h2>Rick Sanchez</h2>
+function Card(props) {
+  //console.log(props)
 
-          <div className="tags">
-            <div className="tag">Status: Vivo</div>
-            <div className="tag">Espécie: Humana</div>
-            <div className="tag">Origem: Terra C-137</div>
-          </div>
-          
-            <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
-          </div>
-        </>
-      )
+  const item = props.item
+
+  //console.log(item)
+
+  return (
+    <>
+      <div className="card">
+        <h2>{item.name}</h2>
+
+        <div className="tags">
+          <div className="tag">Status: Vivo</div>
+          <div className="tag">Espécie: Humana</div>
+          <div className="tag">Origem: Terra C-137</div>
+        </div>
+
+        <img src={item.image} />
+      </div>
+    </>
+  )
 }
 
 export default Card
